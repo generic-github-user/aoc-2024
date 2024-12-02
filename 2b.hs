991486077a96f33451490a036ce33fccce22f83c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2024-12-02 00:42:40 -0500

    day 2, part 2

---
+++ 2b.hs
@@ -0,0 +1,10 @@
+import Data.List
+
+diff [] = []
+diff xs = zipWith (-) (tail xs) xs
+-- almostAll p = (<= 2) . length . filter (not . p)
+alts xs = zipWith mappend (inits xs) (tail (tails xs)) ++ [xs]
+main = do
+  rows <- map (map read . words) . lines <$> readFile "./2.txt" :: IO [[Int]]
+  print $ length $ filter (any (\x -> let d = diff x in
+               (all (< 0) d || all (> 0) d) && all ((`elem` [1..3]) . abs) d) . alts) rows
