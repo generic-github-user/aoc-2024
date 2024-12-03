947a06375d5642909af43f3b7df8a97d86c6b2df
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2024-12-03 00:34:09 -0500

    cleanup

--- 2.hs
+++ 2.hs
@@ -1,7 +1,8 @@
 diff [] = []
 diff xs = zipWith (-) (tail xs) xs
--- main = print $ diff [1..7]
 main = do
   rows <- map (map read . words) . lines <$> readFile "./2.txt" :: IO [[Int]]
-  -- print $ head rows
-  print $ length $ filter (\x -> let d = diff x in (all (< 0) d || all (> 0) d) && all ((`elem` [1..3]) . abs) d) rows
+  print $ length $ filter (\x -> let d = diff x in
+    (all (< 0) d || all (> 0) d) && all ((`elem` [1..3]) . abs) d) rows
+
+
