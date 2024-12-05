948d6460193c72d6f7fa13aa200bb4090355dc70
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2024-12-05 12:58:54 -0500

    fix/clean up day 5, part 1

--- 5.hs
+++ 5.hs
@@ -1,14 +1,12 @@
 import Data.List
 import Data.List.Split
--- import Data.Map (Map)
 import Data.Set (Set, fromList, member)
 
 parse :: [String] -> (Set (Int, Int), [[Int]])
 parse s = (\[left, right] -> (fromList $ map ((\[a, b] -> (a, b)) . map read . splitOn "|") left :: Set (Int, Int),
                              map (map read . splitOn ",") right)) (splitOn [""] s)
 getOrder order x y = if member (x, y) order then LT else if member (y, x) order then GT else EQ
+middle xs = xs !! (length xs `div` 2)
 main = do
   (order, updates) <- parse . lines <$> readFile "./5.txt"
-  print order
-  print updates
-  print $ length $ filter (\u -> sortBy (getOrder order) u == u) updates
+  print $ sum $ map middle $ filter (\u -> sortBy (getOrder order) u == u) updates
