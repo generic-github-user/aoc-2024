a84661399d5ffa717c4e41b17d4e3fb8905d3490
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2024-12-09 23:42:53 -0500

    cleanup (day 8, part 1)

--- 8.hs
+++ 8.hs
@@ -10,8 +10,6 @@ mkArray xs = listArray ((1, 1), (length xs, length (head xs))) (concat $ transpo
 (x1, y1) <+> (x2, y2) = (x1 + x2, y1 + y2)
 (x1, y1) <-> (x2, y2) = (x1 - x2, y1 - y2)
 findAll x = map fst . filter ((== x) . snd)
--- combinations n xs = filter ((== n) . length . nub) (sequence (replicate n xs))
--- combinations xs = concat $ map (zipWith (\a b -> [a, b]) xs) (tail (tails xs))
 combinations xs = concat $ map (zipWith (,) xs) (tail (tails xs))
 antinodes m letter = case (M.lookup letter m) of
   Just v -> (fromList $ concatMap (\(a, b) ->
@@ -22,10 +20,4 @@ main = do
   grid <- mkArray . lines <$> readFile "./8.txt"
   let m = M.fromListWith (++) (map (fmap pure . swap) (filter ((/= '.') . snd) $ assocs grid))
   let allAntinodes = S.filter (inRange (bounds grid)) (unions (map (antinodes m) (M.keys m)))
-  print $ M.keys m
-  print m
-  print allAntinodes
-  print grid
-  print $ antinodes m 'A'
-  -- print (assocs grid)
   print $ size allAntinodes
